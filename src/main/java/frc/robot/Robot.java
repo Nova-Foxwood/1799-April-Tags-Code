858baff3f7da214c9b099d35f2b0631649aea056
Nova-Foxwood <chaffinsydney@gmail.com>
2024-01-13 14:40:18 -0700

    Limelight only nothing else

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -19,39 +19,7 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
  * project.
  */
 public class Robot extends TimedRobot {
-  private Command m_autonomousCommand;
-// Constants such as camera and target height stored. Change per robot and goal!
-    final double CAMERA_HEIGHT_METERS = Units.inchesToMeters(24);
-    final double TARGET_HEIGHT_METERS = Units.feetToMeters(5);
-    // Angle between horizontal and the camera.
-    final double CAMERA_PITCH_RADIANS = Units.degreesToRadians(0);
-
-    // How far from the target we want to be
-    final double GOAL_RANGE_METERS = Units.feetToMeters(3);
-
-    // Change this to match the name of your camera
-    LimeLight camera = new LimeLight("limelightvision");
-
-    // PID constants should be tuned per robot
-    final double LINEAR_P = 0.1;
-    final double LINEAR_D = 0.0;
-    PIDController forwardController = new PIDController(LINEAR_P, 0, LINEAR_D);
-
-    final double ANGULAR_P = 0.1;
-    final double ANGULAR_D = 0.0;
-    PIDController turnController = new PIDController(ANGULAR_P, 0, ANGULAR_D);
-
-    XboxController xboxController = new XboxController(0);
-
-    // Drive motors
-    PWMVictorSPX leftMotor = new PWMVictorSPX(0);
-    PWMVictorSPX rightMotor = new PWMVictorSPX(1);
-    DifferentialDrive drive = new DifferentialDrive(leftMotor, rightMotor);
-  private RobotContainer m_robotContainer;
-  /**
-   * This function is run when the robot is first started up and should be used for any
-   * initialization code.
-   */
+ 
   @Override
   public void robotInit() {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
@@ -121,35 +89,9 @@ public class Robot extends TimedRobot {
   /**
    * This function is called periodically during operator control.
    */
- @Override
-    public void teleopPeriodic() {
-        double forwardSpeed;
-        double rotationSpeed;
-
-        forwardSpeed = -xboxController.getRightY();
-
-        if (xboxController.getAButton()) {
-            // Vision-alignment mode
-            // Query the latest result from PhotonVision
-            var result = camera.getLatestResult();
-
-            if (result.hasTargets()) {
-                // Calculate angular turn power
-                // -1.0 required to ensure positive PID controller effort _increases_ yaw
-                rotationSpeed = -turnController.calculate(result.getBestTarget().getYaw(), 0);
-            } else {
-                // If we have no targets, stay still.
-                rotationSpeed = 0;
-            }
-        } else {
-            // Manual Driver Mode
-            rotationSpeed = xboxController.getLeftX();
-        }
-
-        // Use our forward/turn speeds to control the drivetrain
-        drive.arcadeDrive(forwardSpeed, rotationSpeed);
-
-    }
+   @Override
+  public void teleopPeriodic() {
+  }
   @Override
   public void testInit() {
     // Cancels all running commands at the start of test mode.
